62e80bd491f31588096c4c68bdfdd70e1480114d
Robin Raabe <robinnraabe@gmail.com>
2023-08-29 13:55:36 -0500

    primary work done

--- assignment/scripts/function-practice.js
+++ assignment/scripts/function-practice.js
@@ -15,47 +15,75 @@ console.log('Test - should say "Hello World!"', hello());
 // 2. Function to return an personalized hello, using the `name` argument.
 //    for example 'Hello, Jo!', or 'Hello, Stacy!'
 function helloName(name) {
-  return;
+  return 'Hello, ' + name + '!';
 }
 // Remember to call the function to test
-
+console.log(helloName('Robin'));
 
 // 3. Function to add two numbers together & return the result
-function addNumbers(firstNumber) {
+function addNumbers(firstNumber, secondNumber) {
   // return firstNumber + secondNumber;
+  return firstNumber + secondNumber;
 }
-
+console.log('The sum of 3 + 4 is', addNumbers(3, 4));
 
 // 4. Function to multiply three numbers & return the result
-function multiplyThree() {
-
+function multiplyThree(firstNumber, secondNumber, thirdNumber) {
+  let product = firstNumber * secondNumber * thirdNumber;
+  return product;
 }
-
+console.log('The product of 3 * 4 * 5 is', multiplyThree(3, 4, 5));
 
 // 5. Function that will return true if a number is positive, 
 //    or greater than zero, and false otherwise
 function isPositive(number) {
   if (number > 0) {
-    return;
+    return true;
   }
-  return;
+  else {
+    return false;
+  }
 }
 // Call the function to test each outcome (true & false) 
 // Write a separate console.log statement for each outcome
-
+console.log('Is 4 a positive number?', isPositive(4));
+console.log('If 0 a positive number?', isPositive(0));
 
 // 6. Function to return the _last_ item in an array. If the 
 //    array is empty, return `undefined`.
 function getLast(array) {
+  if (array == []) {
+    return 'undefined';
+  }
+  else {
+  return array[array.length-1];
+  }
 
 }
+console.log('The last item in the array is', getLast([1,2,3,4,5]));
+console.log('The last item in the array is', getLast(['blue', 'pink', 'yellow', 'peach']));
+console.log('The last item in the array is', getLast([]));
 
 // 7. Function to find a value in an array. Return true if the 
 //    value is found and false otherwise. Use a loop;
 //    DO NOT use Array.includes, Array.indexOf, or Array.find 
 function find(value, array) {
-
+  let index = 0;
+  for (let item of array) {
+    if (item == value) {
+      return true;
+      break;
+    }
+    else {
+      if (index == array.length - 1) {
+        return false;
+      }
+    }
+    index++;
+  }
 }
+console.log('Is "peach" included in the array?', find('peach',['blue', 'pink', 'yellow', 'peach']));
+console.log('Is "orange" included in the array?', find('orange',['blue', 'pink', 'yellow', 'peach']));
 
 // ----------------------
 // Stretch Goals
